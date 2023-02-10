35afb07e26a2b19a9edc5c02ec0ab090329ad8cd
tomekskuta <tomasz.skuta@monterail.com>
2023-02-10 10:13:41 +0100

    [chore] add dev and debug options to nuxt.config, add singleAttributePerLine attr to .prettierrc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  dev: process.env.NODE_ENV !== 'production',
+  debug: process.env.NODE_ENV !== 'production',
   typescript: {
     typeCheck: true,
   },
