58510264ebe30ef73490ee035992fc5d52e2e661
Marcin Nieznaj <marcin.nieznaj@monterail.com>
2023-02-14 09:24:15 +0100

    overwrote tailwind colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,22 +1,20 @@
 import type { Config } from 'tailwindcss';
 export default <Partial<Config>>{
   theme: {
-    extend: {
-      colors: {
-        'blue-100': '#8C8EFF',
-        'blue-200': '#595CFF',
-        'blue-300': '#383BE4',
-        green: '#00CB6F',
-        orange: '#FFBC2C',
-        red: '#EE7764',
-        white: '#FFFFFF',
-        'gray-100': '#F9F9FB',
-        'gray-200': '#F0F0F5',
-        'gray-300': '#E6E6EB',
-        'gray-400': '#AFB1BD',
-        'gray-500': '#82828B',
-        black: '#000000',
-      },
+    colors: {
+      'blue-100': '#8C8EFF',
+      'blue-200': '#595CFF',
+      'blue-300': '#383BE4',
+      green: '#00CB6F',
+      orange: '#FFBC2C',
+      red: '#EE7764',
+      white: '#FFFFFF',
+      'gray-100': '#F9F9FB',
+      'gray-200': '#F0F0F5',
+      'gray-300': '#E6E6EB',
+      'gray-400': '#AFB1BD',
+      'gray-500': '#82828B',
+      black: '#000000',
     },
   },
 };
