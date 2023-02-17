08661068dc809d73645d4cb81d035af36a1c82f6
tomekskuta <tomasz.skuta@monterail.com>
2023-02-17 14:39:24 +0100

    [ZFE-27] Add product tile

---
+++ components/ProductTile.vue
@@ -0,0 +1,42 @@
+<template>
+  <nuxt-link
+    class="group block hover:shadow-2 hover:bg-white transition-all duration-200"
+    :to="`/fleet/${zeppelin.id}`"
+  >
+    <img
+      class="rounded w-[260px] h-[260px] object-cover"
+      :src="zeppelin.image.path"
+      :alt="zeppelin.image.alt"
+      :loading="lazyImage ? 'lazy' : 'eager'"
+    />
+    <div class="relative pt-5 px-2.5 pb-9 text-black font-medium">
+      <div class="pb-2.5 text-lg leading-6">
+        {{ zeppelin.name }}
+      </div>
+      <div class="text-sm leading-4">
+        From <strong>${{ zeppelin.lowestPrice }}</strong>
+      </div>
+
+      <div
+        class="opacity-0 duration-200 transition-opacity group-hover:opacity-100 flex absolute w-11 h-11 rounded-full bg-blue-300 justify-center items-center -top-[22px] right-4"
+      >
+        <Arrow class="w-6 h-6 arrow-white rotate-180" />
+      </div>
+    </div>
+  </nuxt-link>
+</template>
+
+<script setup lang="ts">
+import Arrow from '@/assets/icons/arrow_thin_left.svg?skipsvgo';
+
+defineProps<{
+  zeppelin: ZeppelinTile;
+  lazyImage?: boolean;
+}>();
+</script>
+
+<style scoped>
+.arrow-white :deep(path) {
+  @apply fill-white;
+}
+</style>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,12 @@
 import type { Config } from 'tailwindcss';
 export default <Partial<Config>>{
   theme: {
+    boxShadow: {
+      1: '0px 3px 52px rgba(90, 92, 102, 0.06)',
+      2: '3px 7px 18px rgba(103, 105, 116, 0.0833424)',
+      3: '-5px 10px 52px rgba(90, 92, 102, 0.06)',
+      4: '-10px 5px 18px rgba(103, 105, 116, 0.0833424)',
+    },
     colors: {
       'blue-100': '#8C8EFF',
       'blue-200': '#595CFF',
