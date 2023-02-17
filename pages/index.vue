598673a6840b283208d02c5f95fc901259584767
tomekskuta <tomasz.skuta@monterail.com>
2023-02-17 15:57:00 +0100

    [ZFE-7] Use ProductsList on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="w-full grid grid-cols-2 gap-y-7 gap-x-10 grid-rows-2 my-8">
+  <div class="w-full grid grid-cols-2 gap-y-7 gap-x-10 grid-rows-2 mt-8 mb-20">
     <RedirectionBanner
       link="/"
       header="Best zeppelins"
       subheader="Lowest prices"
-      :backgroundImage="backgroundImage"
+      :background-image="backgroundImage"
       :cta="{ title: 'Check out our flying boys', href: '#', isButton: true }"
       class="col-span-2 h-56"
     />
     <RedirectionBanner
       link="/"
       header="Company with decades of history"
-      :backgroundImage="backgroundImage"
+      :background-image="backgroundImage"
       transparent-background
       :cta="{ title: 'Read more', href: '#' }"
       class="col-span-1 h-56"
@@ -19,10 +19,16 @@
     <RedirectionBanner
       link="/"
       header="Need help?"
-      :backgroundImage="backgroundImage"
+      :background-image="backgroundImage"
       transparent-background
       :cta="{ title: 'Contact us', href: '#' }"
       class="col-span-1 h-56"
+    />
+
+    <ProductsList
+      class="col-span-2 mt-14"
+      title="Most popular"
+      :zeppelins="zeppelins"
     />
   </div>
 </template>
@@ -32,4 +38,43 @@ const backgroundImage = {
   path: 'https://imgs.smoothradio.com/images/191589?width=1200&crop=16_9&signature=GRazrMVlAISqkcXrrNA6ku356R0=',
   alt: 'Zeppelin image',
 };
+
+const zeppelins: ZeppelinTile[] = [
+  {
+    id: 1,
+    image: {
+      alt: 'Name',
+      path: 'https://picsum.photos/300',
+    },
+    lowestPrice: 213,
+    name: 'Name',
+  },
+  {
+    id: 2,
+    image: {
+      alt: 'Led Zeppelin',
+      path: 'https://picsum.photos/260',
+    },
+    lowestPrice: 721,
+    name: 'Led Zeppelin',
+  },
+  {
+    id: 3,
+    image: {
+      alt: 'Ten niemiecki',
+      path: 'https://picsum.photos/320',
+    },
+    lowestPrice: 372,
+    name: 'Ten niemiecki',
+  },
+  {
+    id: 4,
+    image: {
+      alt: 'Jeszcze jeden',
+      path: 'https://picsum.photos/360',
+    },
+    lowestPrice: 137,
+    name: 'Jeszce jeden',
+  },
+];
 </script>
