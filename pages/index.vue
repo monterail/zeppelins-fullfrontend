01824631b2f7e60df2ecc42f5fdb6b3206eca92f
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2023-02-20 16:08:55 +0100

    cleanup

--- pages/index.vue
+++ pages/index.vue
@@ -25,20 +25,9 @@
       class="col-span-1 h-56"
     />
   </div>
-  <div>
-    <BaseButton variant="fill">Tets</BaseButton>
-    <BaseButton variant="outline">Tets</BaseButton>
-    <BaseButton to="/">Tets</BaseButton>
-    <BaseButton href="/home">Tets</BaseButton>
-    <BaseButton type="round">A</BaseButton>
-    <BaseButton type="square">B</BaseButton>
-  </div>
 </template>
 
 <script lang="ts" setup>
-import BaseButton from '~/components/Base/Button.vue';
-import CartIcon from '~/assets/icons/cart-icon.svg';
-
 const backgroundImage = {
   path: 'https://imgs.smoothradio.com/images/191589?width=1200&crop=16_9&signature=GRazrMVlAISqkcXrrNA6ku356R0=',
   alt: 'Zeppelin image',
