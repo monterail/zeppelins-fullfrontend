538697a37655742dfb9faebc71eac06363aae88f
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2023-02-23 16:35:14 +0100

    restore ProductList

--- pages/index.vue
+++ pages/index.vue
@@ -24,8 +24,13 @@
       :cta="{ title: 'Contact us', href: '#' }"
       class="col-span-1 h-56"
     />
+    <ProductsList
+      v-if="zeppelins"
+      class="col-span-2 mt-14"
+      title="Most popular"
+      :zeppelins="zeppelins"
+    />
   </div>
-  <HomePopular />
 </template>
 
 <script lang="ts" setup>
@@ -33,4 +38,5 @@ const backgroundImage = {
   path: 'https://imgs.smoothradio.com/images/191589?width=1200&crop=16_9&signature=GRazrMVlAISqkcXrrNA6ku356R0=',
   alt: 'Zeppelin image',
 };
+const { data: zeppelins } = useProductList(4);
 </script>
