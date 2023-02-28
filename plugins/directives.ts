4d7b4b10433e3c33417568b1bd0467d146b649a6
Patryk Wawrzeński <patryk.wawrzenski@monterail.com>
2023-02-28 09:51:25 +0100

    fix directives function naming

--- plugins/directives.ts
+++ plugins/directives.ts
@@ -8,7 +8,7 @@ import {
 export default defineNuxtPlugin((nuxt) => {
   const LOADING_CLASS = 'v-loading';
 
-  const setLoadingClass = (el: HTMLElement, value: LoadingValue) => {
+  const toggleLoadingClass = (el: HTMLElement, value: LoadingValue) => {
     if (value || value === undefined) {
       el.classList.add(LOADING_CLASS);
     } else {
@@ -18,10 +18,10 @@ export default defineNuxtPlugin((nuxt) => {
 
   const vLoading: LoadingDirective = {
     mounted(el, { value }) {
-      setLoadingClass(el, value);
+      toggleLoadingClass(el, value);
     },
     updated(el, { value }) {
-      setLoadingClass(el, value);
+      toggleLoadingClass(el, value);
     },
     getSSRProps({ value }: LoadingBinding) {
       return {
