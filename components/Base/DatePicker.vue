c3ec8de12fdc6af6895a07400b6fc3609879f843
Jakub Zakrzewski <82118539+zakrzaq@users.noreply.github.com>
2023-03-15 14:01:13 +0100

    [ZFE-16] Checkout Page (#19)
    * structure
    
    * page layout
    base counter
    base date picker
    checkout date selector
    
    * radio options
    
    * radio options
    
    * file upload
    controls
    
    * cleanup
    
    * refactor watch to @update:
    
    * summary view
    
    * summary card
    refactor on emits/props
    
    * ..
    
    * File upload drag and drop
    
    * refactor: project component structure, move all component to the corresponding folders & update usage (#22)
    
    * refactor: move Footer & Header component to the layout folder
    
    * fix(layout): fix default layout Header & Footer components usage
    
    * refactor(Products): refactor ProductsList component, rename to List and move to the Product folder
    
    * refactor(Products): refactor ProductTile component, rename to Tile and move to the Product folder
    
    * fix(index): fix usage of ProductList component
    
    * refactor(RedirectionBanner): move to the Base folder
    
    * fix(index): update RedirectionBanner component usage
    
    ---------
    
    Co-authored-by: Elmir Ismayilzada <mrelmirismayilzade@gmail.com>
    
    * ctsList -> ProductList
    
    * formatting;
    file upload: action.prevent
    
    * cr fixes:
    vounter != 0
    formatting
    RadioGroup prop name
    
    * [ZFE-38] Add Modals - prototype (#9)
    
    * rebase from main
    
    * base input added
    
    * base button inner positioning fix
    
    * add AuthModal component to the default layout
    
    * add modal loading state
    
    * remove redundant styling from AuthModal
    
    * add prototype user signup and login
    
    * code readability cleanup
    
    * add user database for test_profiles table
    
    * add LoginForm and SignupForm components to AuthModal
    
    * add content variant to BaseButton
    
    * add BaseButton to BaseModal
    
    * move close-icon.svg file to correct directory
    
    * add close icon svg component to BaseModal
    
    * remove BaseButton content variant
    
    * add hover animation to BaseModal close button
    
    * adjust logic regarding database changes
    
    ---------
    
    Co-authored-by: Marcin Nieznaj <marcin.nieznaj@monterail.com>
    Co-authored-by: Patryk Wawrzeński <patryk.wawrzenski@monterail.com>
    
    ---------
    
    Co-authored-by: Elmir Ismayilzada <103642381+elmir-ismayilzada@users.noreply.github.com>
    Co-authored-by: Elmir Ismayilzada <mrelmirismayilzade@gmail.com>
    Co-authored-by: Marcin <45370709+mnieznaj@users.noreply.github.com>
    Co-authored-by: Marcin Nieznaj <marcin.nieznaj@monterail.com>
    Co-authored-by: Patryk Wawrzeński <patryk.wawrzenski@monterail.com>

---
+++ components/Base/DatePicker.vue
@@ -0,0 +1,30 @@
+<template>
+  <label
+    :for="name"
+    class="inline-block min-w-[250px]"
+  >
+    <slot class="block mb-7" />
+    <input
+      :id="`date-picker__${name}`"
+      type="date"
+      :name="name"
+      :value="currentDate"
+      class="block p-3 border rounded border-gray-300 text-xs w-full max-w-md"
+      @change="handleChange"
+    />
+  </label>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  name?: string;
+  disabled?: boolean;
+}>();
+
+const emit = defineEmits(['update:date']);
+
+const currentDate = new Date().toJSON().slice(0, 10);
+
+const handleChange = ($event: Event) =>
+  emit('update:date', ($event.target as HTMLInputElement).value);
+</script>
---
+++ components/Checkout/Summary.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="w-full max-h-[405px] border border-gray-200 rounded-[5px] p-5">
+    <h4>Order summary</h4>
+    <div
+      v-if="product"
+      class="flex mt-10"
+    >
+      <img
+        v-if="product.image && product.name"
+        class="w-[100px] h-[100px] mr-5 rounded-[4px]"
+        :src="product.image"
+        :alt="product.name"
+      />
+      <div>
+        <p class="text">{{ product.name || 'Name' }}</p>
+        <p class="font-semibold">${{ product.price }}</p>
+      </div>
+    </div>
+    <ul class="mt-10">
+      <li
+        v-if="order.insurance"
+        class="w-full flex justify-between py-4 border-t border-t-gray-200"
+      >
+        <p v-if="order?.insurance?.title">
+          {{ order.insurance.title }}
+        </p>
+        <p class="font-bold">${{ order.insurance.detail }}</p>
+      </li>
+      <li
+        v-else
+        class="w-full flex justify-between py-4 border-t border-t-gray-200"
+      >
+        <p>No insurance</p>
+        <p class="font-bold">$0</p>
+      </li>
+      <li class="w-full flex justify-between py-4 border-t border-gray-300">
+        <p>{{ order.days }} days</p>
+        <p class="font-bold">${{ reservationCost }}</p>
+      </li>
+      <li class="w-full flex justify-between py-4 border-t border-t-gray-200">
+        <p>Total</p>
+        <p class="font-bold">${{ total }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Order } from '~/types/order';
+
+const props = defineProps<{
+  order: Order;
+}>();
+
+const { data: product } = useProductById('1');
+
+const reservationCost = computed(() => {
+  return props.order.days && product.value && product.value.price
+    ? props.order.days * +product.value.price
+    : 0;
+});
+
+const total = computed(() => {
+  return props.order.insurance?.detail
+    ? reservationCost.value + parseInt(props.order.insurance.detail)
+    : reservationCost.value;
+});
+</script>
