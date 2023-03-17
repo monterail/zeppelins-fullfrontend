cddfd9c6bfeae124c6721e9ac176f3e1cec41015
Elmir Ismayilzada <mrelmirismayilzade@gmail.com>
2023-03-17 15:18:20 +0100

    feat(profile): add profile order card component

---
+++ components/Profile/Orders/Card.vue
@@ -0,0 +1,27 @@
+<template>
+  <BaseCard :title="title">
+    <div v-if="orders.length">
+      <div
+        v-for="(order, index) in orders"
+        :key="order.id"
+        class="min-h-[100px] border-gray-300 pt-2.5"
+        :class="{
+          'border-b pb-2.5': index !== orders.length - 1,
+          '!pt-0': index === 0,
+          '!pb-0': index === orders.length - 1,
+        }"
+      >
+        <ProfileOrdersOrder :order="order" />
+      </div>
+    </div>
+  </BaseCard>
+</template>
+
+<script lang="ts" setup>
+import { ProfileOrder } from '~/types/order';
+
+defineProps<{
+  title: string;
+  orders?: ProfileOrder[];
+}>();
+</script>
