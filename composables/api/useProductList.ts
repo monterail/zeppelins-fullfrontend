196fb41d8afbd7bd107799b9af6cb9b47e54beeb
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2023-03-17 11:48:58 +0100

    cleanup update documentation covert types ifor vueQuery

--- composables/api/useProductList.ts
+++ composables/api/useProductList.ts
@@ -1,21 +1,25 @@
 import { useQuery } from 'vue-query';
 import type { Database } from '~/types/generated-types';
+import type { FullProduct } from '~/types/products';
 
 export const useProductList = (count = Infinity) => {
   const client = useSupabaseClient<Database>();
 
-  const result = useQuery(['productsList', count], async () => {
-    const { data, error } = await client
-      .from('products')
-      .select('*, product_specifications (*)')
-      .limit(count);
+  const result = useQuery(
+    ['productsList', count],
+    async (): Promise<FullProduct[]> => {
+      const { data, error } = await client
+        .from('products')
+        .select('*, product_specifications (*)')
+        .limit(count);
 
-    if (error) {
-      console.error(error);
-      throw new Error(error.message);
-    }
-    return data;
-  });
+      if (error) {
+        console.error(error);
+        throw new Error(error.message);
+      }
+      return data as FullProduct[];
+    },
+  );
 
   onServerPrefetch(async () => {
     await result.suspense();
--- types/products.ts
+++ types/products.ts
@@ -3,6 +3,7 @@ import { Database } from '~/types/generated-types';
 export type Product = Database['public']['Tables']['products']['Row'];
 export type ProductSpecifications =
   Database['public']['Tables']['product_specifications']['Row'];
+
 export interface FullProduct extends Product {
   product_specifications: ProductSpecifications;
 }
