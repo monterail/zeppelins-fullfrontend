189577d093919493296237714cb43c79f41ad7e6
Elmir Ismayilzada <mrelmirismayilzade@gmail.com>
2023-03-17 15:24:45 +0100

    feat(profile): add profile details page

---
+++ pages/profile/details.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Welcome back USER!</h1>
+  <div class="mt-10 grid grid-cols-[1fr_3fr] gap-36">
+    <ProfileSidebar />
+    <ProfileContentDetails />
+  </div>
+</template>
+<script lang="ts" setup>
+definePageMeta({
+  middleware: ['auth'],
+});
+</script>
