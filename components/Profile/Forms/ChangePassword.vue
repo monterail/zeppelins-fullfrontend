ca39b2c9c2b1743d0cae710c07b2e3efa5c31228
Elmir Ismayilzada <mrelmirismayilzade@gmail.com>
2023-03-17 15:23:26 +0100

    feat(profile): add profile change password form component

---
+++ components/Profile/Forms/ChangePassword.vue
@@ -0,0 +1,41 @@
+<template>
+  <BaseCard title="Change password">
+    <form
+      class="max-w-[500px]"
+      @submit.prevent
+    >
+      <BaseInput
+        id="password"
+        v-model="formData.password"
+        type="password"
+        label="Password"
+        placeholder="Enter your current password"
+      />
+      <BaseInput
+        id="newPassword"
+        v-model="formData.password"
+        type="password"
+        label="New password"
+        placeholder="Enter your new password"
+      />
+      <BaseButton
+        class="mt-3 w-40"
+        @click.prevent="saveInformation"
+      >
+        Save
+      </BaseButton>
+    </form>
+  </BaseCard>
+</template>
+
+<script setup lang="ts">
+const formData = ref({
+  password: '',
+  newPassword: '',
+});
+
+const saveInformation = () => {
+  // eslint-disable-next-line no-console
+  console.log(formData.value);
+};
+</script>
