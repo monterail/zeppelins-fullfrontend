7385d84f9d3e6af14bc45515ced0918a4e738983
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2023-03-17 10:46:04 +0100

    use supabse types to construct interfaces

--- composables/api/useProductList.ts
+++ composables/api/useProductList.ts
@@ -1,25 +1,21 @@
 import { useQuery } from 'vue-query';
 import type { Database } from '~/types/generated-types';
-import { FullProduct } from '~~/types/products';
 
 export const useProductList = (count = Infinity) => {
   const client = useSupabaseClient<Database>();
 
-  const result = useQuery(
-    ['productsList', count],
-    async (): Promise<FullProduct[]> => {
-      const { data, error } = await client
-        .from('products')
-        .select('*, product_specifications (*)')
-        .limit(count);
+  const result = useQuery(['productsList', count], async () => {
+    const { data, error } = await client
+      .from('products')
+      .select('*, product_specifications (*)')
+      .limit(count);
 
-      if (error) {
-        console.error(error);
-        throw new Error(error.message);
-      }
-      return data as FullProduct[];
-    },
-  );
+    if (error) {
+      console.error(error);
+      throw new Error(error.message);
+    }
+    return data;
+  });
 
   onServerPrefetch(async () => {
     await result.suspense();
--- types/products.ts
+++ types/products.ts
@@ -1,18 +1,8 @@
-export interface Product {
-  created_at?: string | null;
-  description?: string | null;
-  id?: string;
-  image?: string | null;
-  name?: string | null;
-  price?: number | null;
-}
+import { Database } from '~/types/generated-types';
 
-export interface ProductSpecifications {
-  cabins?: number | null;
-  product_id?: string | null;
-  range?: number | null;
-}
-
+export type Product = Database['public']['Tables']['products']['Row'];
+export type ProductSpecifications =
+  Database['public']['Tables']['product_specifications']['Row'];
 export interface FullProduct extends Product {
   product_specifications: ProductSpecifications;
 }
