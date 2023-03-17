f1eaf640fec291ded11dcdb7260419361dfe5b54
Jakub Zakrzewski <j.zakrzewsky@gmail.com>
2023-03-17 14:08:34 +0100

    useInsuranceOptions apply above

--- components/Checkout/Insurance.vue
+++ components/Checkout/Insurance.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <p class="mb-7 text-2xl">Insurance</p>
+    <div v-if="isLoading">...</div>
     <BaseRadioGroup
+      v-else-if="insuranceOptions && selectedInsurance"
       v-model:selected="selectedInsurance"
       :options="insuranceOptions"
-      :plans="insuranceOptions"
       @update:selected="$emit('update:insurance', $event)"
     />
   </div>
@@ -13,28 +14,8 @@
 <script setup lang="ts">
 defineEmits(['update:insurance']);
 
-// TODO: replace wit API data
-const insuranceOptions = ref([
-  {
-    id: 1,
-    title: 'No Insurance',
-    description: "Don't tell us we didn't warn you",
-    detail: '0',
-  },
-  {
-    id: 2,
-    title: 'Basic Insurance',
-    description: 'Just the basics, better be careful anyway',
-    detail: '99',
-  },
-  {
-    id: 3,
-    title: 'Full Insurance',
-    description: 'Safe from everything, including sharnados',
-    detail: '199',
-  },
-]);
-
-// This needs to be passed to radio group, always first item of array
-const selectedInsurance = ref(insuranceOptions.value[0]);
+const { data: insuranceOptions, isLoading } = useInsuranceOptions();
+const selectedInsurance = ref(
+  insuranceOptions.value ? insuranceOptions.value[0] : null,
+);
 </script>
