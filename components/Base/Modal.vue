53f833fc9842b15fffef6f55271e4eaf1be175ba
Patryk Wawrzeński <patryk.wawrzenski@monterail.com>
2023-03-20 15:06:50 +0100

    fix tailwind opacity in Modal and RedirectionBanner

--- components/Base/Modal.vue
+++ components/Base/Modal.vue
@@ -18,7 +18,7 @@
         leave-from="opacity-100"
         leave-to="opacity-0"
       >
-        <div class="bg-opacity-/25 fixed inset-0 bg-black" />
+        <div class="fixed inset-0 bg-black/25" />
       </TransitionChild>
 
       <div class="fixed inset-0 overflow-y-auto">
