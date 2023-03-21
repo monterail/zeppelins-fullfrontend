20c02d32f9bfae0919b547968489d6f9b42d858a
Elmir Ismayilzada <mrelmirismayilzade@gmail.com>
2023-03-21 11:29:55 +0100

    fix(ProfileOrdersCard): set empty array as default orders list & update navigation styles

--- components/Profile/Orders/Card.vue
+++ components/Profile/Orders/Card.vue
@@ -2,12 +2,9 @@
   <BaseCard :title="title">
     <div v-if="orders.length">
       <div
-        v-for="(order, index) in orders"
+        v-for="order in orders"
         :key="order.id"
-        class="min-h-[100px] border-gray-300 pt-2.5 first:pt-0 last:pb-0"
-        :class="{
-          'border-b pb-2.5': index !== orders.length - 1,
-        }"
+        class="min-h-[100px] border-b border-gray-300 py-2.5 first:pt-0 last:border-0 last:pb-0"
       >
         <ProfileOrdersOrder :order="order" />
       </div>
@@ -18,8 +15,13 @@
 <script lang="ts" setup>
 import { ProfileOrder } from '~/types/order';
 
-defineProps<{
-  title: string;
-  orders?: ProfileOrder[];
-}>();
+withDefaults(
+  defineProps<{
+    title: string;
+    orders?: ProfileOrder[];
+  }>(),
+  {
+    orders: () => [],
+  },
+);
 </script>
