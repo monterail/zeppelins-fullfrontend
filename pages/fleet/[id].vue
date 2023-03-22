4ae4f11c92186f319a3c1a68ceb8bbb0dc07d41b
Patryk Wawrzeński <patryk.wawrzenski@monterail.com>
2023-03-22 14:54:28 +0100

    fix individual fleet page

--- pages/fleet/[id].vue
+++ pages/fleet/[id].vue
@@ -1,63 +1,60 @@
 <template>
-  <div v-if="zeppelin">
-    <div class="mb-10 mt-7 text-xs">
-      Category / <span class="font-semibold">{{ zeppelin.name }}</span>
-    </div>
-    <div class="mb-44 grid grid-cols-2 gap-x-16 gap-y-5">
-      <img
-        :src="zeppelin.image || '~/assets/images/deafult_airship.JPG'"
-        :alt="zeppelin.name || 'Default zeppelin image'"
-        class="w-full rounded"
-      />
-      <div class="w-full">
-        <h1 class="mb-2">{{ zeppelin.name }}</h1>
-        <h2 class="mb-2 font-normal">
-          From <span class="font-bold">${{ zeppelin.price }}</span> per day
-        </h2>
-        <hr class="mb-5 border border-gray-300" />
-        <p class="mb-5">
-          {{ zeppelin.description }}
-        </p>
-        <BaseButton
-          class="mb-14 rounded-md bg-blue-200 px-5 py-2 text-white"
-          href="/checkout"
-        >
-          Book
-        </BaseButton>
-        <ul>
-          <BaseCollapsableItem
-            v-for="item in listData"
-            :key="item.id"
-            :info="item"
-            as="li"
-          />
-        </ul>
+  <div>
+    <div v-loading="isLoading">
+      <div class="mb-10 mt-7 text-xs">
+        Category / <span class="font-semibold">{{ zeppelin?.name }}</span>
       </div>
-      <ul class="flex">
-        <li
-          v-for="perk in perks"
-          :key="perk.id"
-          class="flex w-1/3 items-center gap-2"
-        >
-          <component :is="perk.icon"></component>
-          <p class="my-auto text-xs">{{ perk.description }}</p>
-        </li>
-      </ul>
+      <div class="grid min-h-[620px] grid-cols-2 gap-x-16 gap-y-5">
+        <div>
+          <img
+            :src="zeppelin?.image || '~/assets/images/default_zeppelin.jpg'"
+            :alt="zeppelin?.name || 'Zeppelin image'"
+            class="mb-5 w-full rounded"
+          />
+          <ul class="flex">
+            <li
+              v-for="perk in perks"
+              :key="perk.id"
+              class="flex w-1/3 items-center gap-2"
+            >
+              <component :is="perk.icon" />
+              <p class="my-auto max-w-[100px] text-xs">
+                {{ perk.description }}
+              </p>
+            </li>
+          </ul>
+        </div>
+        <div class="w-full">
+          <h1 class="mb-2">{{ zeppelin?.name }}</h1>
+          <h2 class="mb-5 border-b border-gray-300 pb-2 font-normal">
+            From <span class="font-bold">${{ zeppelin?.price }}</span> per day
+          </h2>
+          <p class="mb-5">
+            {{ zeppelin?.description }}
+          </p>
+          <BaseButton
+            class="mb-14 w-[309px] rounded-md bg-blue-200 px-5 py-2 text-white"
+            href="/checkout"
+          >
+            Book
+          </BaseButton>
+          <ul>
+            <BaseCollapsableItem
+              v-for="item in listData"
+              :key="item.id"
+              :info="item"
+              as="li"
+            />
+          </ul>
+        </div>
+      </div>
     </div>
     <ProductList
-      v-if="zeppelins"
       :is-loading="isLoadingPopular"
       title="Others from our fleet"
       :zeppelins="zeppelins"
-      class="mb-40"
+      class="mb-32"
     />
-  </div>
-  <div v-else-if="!isLoading">No zeppelin found...</div>
-  <div
-    v-else
-    :v-loading="isLoading"
-  >
-    We are loading
   </div>
 </template>
 
